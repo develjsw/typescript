435b49c031e0c97e748fe678b2ecaddf3ff0946e
jsw <develjsw@gmail.com>
2024-07-08 19:47:38 +0900

    인덱스 시그니처 예시3

---
+++ 인덱스 시그니처/test/index-signature3.ts
@@ -0,0 +1,12 @@
+interface NewCar {
+    [key: string]: string | number // 인덱스 시그니처 : key는 문자열만 허용, 값은 문자열, 숫자 허용
+}
+
+const myNewCar: NewCar = {
+    type: 'avante',
+    price: 32000000,
+    "color": 'gray'
+}
+
+const hyundai: string = 'type';
+console.log(myNewCar[hyundai]); // myNewCar객체에 동적 속성 접근
