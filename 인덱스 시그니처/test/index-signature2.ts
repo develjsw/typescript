9242e785c52e939436f994070f29df234bc81725
jsw <develjsw@gmail.com>
2024-07-08 19:36:40 +0900

    인덱스 시그니처 예시2

---
+++ 인덱스 시그니처/test/index-signature2.ts
@@ -0,0 +1,13 @@
+interface People {
+    [key: string]: string | number | boolean // 인덱스 시그니처 : key는 문자열만 허용, 값은 문자열, 숫자, 불린 허용
+}
+
+const me: People = {
+    name: 'sangwoo',
+    "age": 26,
+    alive: true
+}
+
+console.log(me['name']);
+console.log(me.age);
+console.log(me['alive']);
