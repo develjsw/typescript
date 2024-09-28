57c9216c31af574c668e5f53d46128559a642d02
jsw <develjsw@gmail.com>
2024-09-28 23:58:10 +0900

    generator 예시1

---
+++ Generator (제너레이터)/test/generator1.ts
@@ -0,0 +1,25 @@
+function* gen(n) {
+    let res;
+    res = yield n;    // n: 0 ⟸ gen 함수에 전달한 인수
+
+    console.log(res); // res: 1 ⟸ 두번째 next 호출 시 전달한 데이터
+    res = yield res;
+
+    console.log(res); // res: 2 ⟸ 세번째 next 호출 시 전달한 데이터
+    res = yield res;
+
+    console.log(res); // res: 3 ⟸ 네번째 next 호출 시 전달한 데이터
+    return res;
+}
+const generatorObj = gen(0);
+
+console.log(generatorObj.next());       // 제너레이터 함수 시작
+console.log(generatorObj.next(1)); // 제너레이터 객체에 1 전달
+console.log(generatorObj.next(2)); // 제너레이터 객체에 2 전달
+console.log(generatorObj.next(3)); // 제너레이터 객체에 3 전달
+/*
+{ value: 0, done: false }
+{ value: 1, done: false }
+{ value: 2, done: false }
+{ value: 3, done: true }
+*/
